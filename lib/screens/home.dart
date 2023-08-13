34ae56b81eab7ef1edc65a34be1a8ec4bae45f01
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-08-13 12:02:35 +0330

    fix gradle and add getx

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_music_app/constant/colors.dart';
-import 'package:flutter_music_app/constant/strings.dart';
 import 'package:flutter_music_app/constant/text_style.dart';
+import 'package:flutter_music_app/controller/player_controller.dart';
+import 'package:get/get.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
+  var controller = Get.put(PlayerController());
 
   @override
   Widget build(BuildContext context) {
@@ -41,47 +44,57 @@ class Home extends StatelessWidget {
             ),
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView.builder(
-            physics: ScrollPhysics(),
-            itemCount: 100,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 5.0, vertical: 3),
-                child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Color.fromARGB(46, 255, 255, 255),
-                    ),
-                    child: const ListTile(
-                        title: Text(
-                          'Music Name',
-                          style: TextStyle(
-                              fontSize: 15,
-                              color: whiteColor,
-                              fontWeight: FontWeight.bold),
+        body: FutureBuilder(
+          builder: (context, snapshot) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.builder(
+                physics: ScrollPhysics(),
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 5.0, vertical: 3),
+                    child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Color.fromARGB(26, 255, 255, 255),
                         ),
-                        subtitle: Text(
-                          'Music Name',
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 218, 211, 211),
-                            fontSize: 12,
-                          ),
-                        ),
-                        leading: CircleAvatar(
-                          backgroundColor: Color.fromARGB(17, 255, 255, 255),
-                          child: Icon(
-                            Icons.music_note,
-                            color: Colors.white,
-                          ),
-                        ),
-                        trailing:
-                            Icon(Icons.play_circle, color: Colors.white))),
-              );
-            },
-          ),
+                        child: const ListTile(
+                            title: Text(
+                              'Music Name',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  color: whiteColor,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text(
+                              'Music Name',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 218, 211, 211),
+                                fontSize: 12,
+                              ),
+                            ),
+                            leading: CircleAvatar(
+                              backgroundColor:
+                                  Color.fromARGB(17, 255, 255, 255),
+                              child: Icon(
+                                Icons.music_note,
+                                color: Colors.white,
+                              ),
+                            ),
+                            trailing:
+                                Icon(Icons.play_circle, color: Colors.white))),
+                  );
+                },
+              ),
+            );
+          },
+          future: controller.audioQuery.querySongs(
+              ignoreCase: true,
+              orderType: OrderType.ASC_OR_SMALLER,
+              sortType: null,
+              uriType: UriType.EXTERNAL),
         ),
       ),
     );
